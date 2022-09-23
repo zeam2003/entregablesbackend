4c7fad40cc874fa67cceefcf102a28aad7bb9fd7
zeam2003 <zeam2003@outlook.com>
2022-09-22 23:23:28 -0300

    entregable Sockets

--- indexhandle.js
+++ indexhandle.js
@@ -1,5 +1,22 @@
+// Express - Http y Sockets
 const express = require('express');
+const {Server: HttpServer, get} = require('http');
+const {Server: SocketServer} = require('socket.io');
+
+//Fs
+const fs = require('fs');
+
+// Express y Socketsx
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new SocketServer(httpServer);
+//const cors = require('cors');
+
+//app.use(cors({ origin: '*'}));
+
+let Mensajes =  [];
+let Productos = [];
+
 const bodyParser = require('body-parser');
 const productosRouter = require('./productos');
 const controlador = require('./productos.controller');
@@ -8,12 +25,36 @@ const handlebars = require('express-handlebars');
 const localDB = new controlador();
 // views
 //app.use('views', './views/');
+ async function obtener() {
+    try {
+       const data = await fs.promises.readFile('./data/productos.json', 'utf-8')
+       Productos = JSON.parse(data)
+       //return console.log(data);
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// Web Sockets
+io.on('connection', (socket) => {
+    obtener();
+    //console.log(`conectado: ${socket.id}`);
+    socket.emit('productos', Productos);
+    socket.emit('mensajes', Mensajes);
+
+    // escucha los mensajes
+    socket.on('new_msg', data => {
+        //console.log(data);
+        Mensajes.push(data);
+        io.sockets.emit('mensajes', Mensajes);
+    });
+});
 
 // view engine
 app.set('view engine', 'hbs');
 const hbs = handlebars.engine({
     extname: 'hbs',
-    layoutsDir: './views/layouts/'
+    layoutsDir: './views/layouts/',
 });
 
 app.engine('hbs', hbs);
@@ -24,9 +65,11 @@ app.get('/registro', (req, res) => {
 });
 
 app.get('/listado', (req, res) => {
-    const data = localDB.getAll();
-    console.log(data);
-    res.render('main', {layout: 'productos'});
+   // const data = localDB.getAll();
+   // console.log(data);
+    obtener();
+    console.log('esto tengo', Productos.length)
+    res.render('main', {layout: 'productos', Productos});
     //res.render('main', {layout: 'productos', data});
 });
 
@@ -41,4 +84,4 @@ app.use('/api/productos', productosRouter);
 app.use(express.static(__dirname + '/public'));
 
 
-app.listen(8080, () => { console.log('Servidor Activo'); });
+httpServer.listen(8080, () => { console.log('Servidor Express + Sockets - Activo'); });
